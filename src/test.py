9a57095bd4d34642fc89b962228d38e6bd42b0fc
Mohammad Momeni <lo3me@pm.me>
2025-07-24 18:51:36 +0200

    Keeping the old tests

--- src/test.py
+++ src/test.py
@@ -1,23 +1,34 @@
-# import os
-# import sys
-# p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# p = os.path.join(p, "providers")
-# sys.path.append(p)
-# print(p)
+# from providers.n8n import N8nAgentLib
+# from providers.awsome_agent import AwsomeAgentLib
+# from providers.aiagentslive import AiAgentsLiveLib
+# from providers.aiagentlist import AiAgentsListLib
 
-from providers.n8n import N8nAgentLib
-from providers.awsome_agent import AwsomeAgentLib
+# from llms import gemini_with_search
+
+# import asyncio
+
+# async def test():
+#     # async with AwsomeAgentLib() as lib:
+#     #     agents = await lib.fetch_new_agents()
+#     # for k, v in agents.items():
+#     #     print(f"{k}:")
+#     #     for agent in v:
+#     #         print(f"{agent.name} - {agent.description_short} ({agent.url})")
+#     #         # print(f"{agent.description_long[:100] if agent.description_long else ''}...\n")
+
+#     async with AiAgentsListLib() as lib:
+#         agents = await lib.fetch_new_agents()
+#     for k, v in agents.items():
+#         print(f"{k}:")
+#         for agent in v:
+#             print(f"{agent.name} - {agent.description_short} ({agent.url})")
+#             print(f"{agent.description_long}\n")
 
 
-import asyncio
+#     # # read the prompt from prompts/gemini_news.txt
+#     # with open("src/prompts/gemini_news.txt", "r") as f:
+#     #     prompt = f.read()
+#     # response = await gemini_with_search(prompt)
+#     # print(response)
 
-async def test():
-    async with AwsomeAgentLib() as lib:
-        agents = await lib.fetch_new_agents()
-    for k, v in agents.items():
-        print(f"{k}:")
-        for agent in v:
-            print(f"{agent.name} - {agent.description_short} ({agent.url})")
-            # print(f"{agent.description_long[:100] if agent.description_long else ''}...\n")
-
-asyncio.run(test())
+# asyncio.run(test())
