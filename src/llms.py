80ad661a38fd40dcabaf52c0f8f14aed9893adb8
Mohammad Momeni <lo3me@pm.me>
2025-07-24 11:17:31 +0200

    Add Gemini provider (functional)

---
+++ src/llms.py
@@ -0,0 +1,25 @@
+from google import genai
+from google.genai import types
+
+async def gemini_with_search(
+    prompt: str,
+    model: str = "gemini-2.5-flash",
+    max_tokens: int = 1000,
+    temperature: float = 0.2,
+    token = None
+):
+    client = genai.Client(api_key=token)
+    grounding_tool = types.Tool(
+        google_search=types.GoogleSearch()
+    )
+    config = types.GenerateContentConfig(
+        tools=[grounding_tool],
+        max_output_tokens=max_tokens,
+        temperature=temperature
+    )
+    response = await client.aio.models.generate_content(
+        model=model,
+        contents=prompt,
+        config=config,
+    )
+    return response.text
