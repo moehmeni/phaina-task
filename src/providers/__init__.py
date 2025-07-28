5b8c9723614f0435450adca6141808cf4c4712e3
Mohammad Momeni <lo3me@pm.me>
2025-07-28 11:41:42 +0200

    Add templates and static assets

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -2,3 +2,7 @@ from .awsome_agent import AwsomeAgentLib
 from .n8n import N8nAgentLib
 from .aiagentslive import AiAgentsLiveLib
 from .aiagentlist import AiAgentsListLib
+from .huggingface import HuggingFacePaperProvider
+from .google_search import get_related_tweet_from_headline
+from .x import get_tweet_html, fetch_tweet_info, env
+from .reddit import fetch_top_reddit_posts
